681d506d4a0794d74a3f5bf80b040aaa0a8d51bf
hannahmixter <hannah.mixter@gmail.com>
2023-01-06 22:13:05 -0600

    added more js

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,10 @@
+function lowToHighArray(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
+}
 const wordLength = 8;
 const choiceArray = [];
 const uppercaseCodes = lowToHighArray(65, 90);
@@ -14,22 +21,51 @@ const specialCharCodes = lowToHighArray(33, 47).concat(
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-function lowToHighArray(low, high) {
-  const array = [];
-  for (let i = low; i <= high; i++) {
-    array.push(i);
-  }
-  return array;
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  const correctPrompts = getPrompts();
+  const passwordText = document.querySelector("#password");
+  
+  if(correctPrompts) {
+    const newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+
+
+
+function generatePassword() {
+  const password = "";
+  for(var i = 0; i < wordLength; i++) {
+    var randomChar = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[randomChar];
+  }
+  return password;
+}
+
+function getPrompts() {
+  choiceArray = [];
+  wordLength = parseInt(prompt("Choose a password length between 8 and 128 characters"));
+  if(isNaN(wordLength) || wordLength < 8 || wordLength > 128) {
+    alert("Password length must be a number (8-128). Please try again");
+    return false;
+  }
+
+  if (confirm("Include lowercase letters?")) {
+    choiceArray = choiceArray.concat(lowercaseCodes);
+  }
+  if (confirm("Include uppercase letters?")) {
+    choiceArray = choiceArray.concat(uppercaseCodes);
+  }
+  if (confirm("Include numbers?")) {
+    choiceArray = choiceArray.concat(numberCodes);
+  }
+  if (confirm("Include special characters?")) {
+    choiceArray = choiceArray.concat(specialCharCodes);
+  }
+  return true;
+}
